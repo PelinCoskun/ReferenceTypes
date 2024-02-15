498865b06ec90d7e56e00e788df909c99d739532
PelinCoskun <pelinefpit@gmail.com>
2024-02-15 22:03:24 +0300

    two

---
+++ Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Customer customer1 = new Customer() { Id=1,FirstName="Pelin",LastName="Coşkun",City="İstanbul"};
+Customer customer3 = new Customer();
+
+Customer customer2 = new Customer(2, "Çado", "Pekto", "İstanbul");
+
+
+class Customer
+{
+    public Customer()
+    {
+        
+    }
+    //default ctor
+    public Customer(int id, string firstName, string lastName,string city)
+    {
+        Console.WriteLine("Yapıcı blok çalıştı");
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
