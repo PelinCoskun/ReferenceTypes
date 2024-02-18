5aef85be655f36da41b3d52670b1c22b64e58f89
PelinCoskun <pelinefpit@gmail.com>
2024-02-18 16:16:53 +0300

    sd

---
+++ Interfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//inteface new'lenmez
+//interfaceler onu impelemente eden classın  referansını tutar
+//IPersonManager customerManager = new CustomerManager();
+
+//IPersonManager employeeManager = new EmployeeManager();
+
+ProjectManager projectManager = new ProjectManager();
+projectManager.Add(new EmployeeManager());
+
+interface IPersonManager
+{
+    //unimplement operation
+    void Add();
+    void Update();
+    
+}
+
+//inherits - class ------------------implements - interface
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        //müşteri ekeleme kodları
+        Console.WriteLine("müşteri eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("müşteri güncellendi");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        //personel ekleme kodları
+        Console.WriteLine("personel eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("personel güncellendi"); ;
+    }
+}
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+
+    
+} 
