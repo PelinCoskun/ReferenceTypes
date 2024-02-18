6d068977dfce0ac59073a0792230edb1b7952237
PelinCoskun <pelinefpit@gmail.com>
2024-02-18 16:45:55 +0300

    gen

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,22 +2,35 @@
 
 List<string> sehirler=new List<string>();
 sehirler.Add("İstanbul");
+sehirler.Add("İstanbul");
+sehirler.Add("İstanbul");
 Console.WriteLine(sehirler.Count);
 
 MyList<string> sehirler2= new MyList<string>();
+sehirler2.Add("İstanbul");
+sehirler2.Add("İstanbul");
+sehirler2.Add("İstanbul");
+sehirler2.Add("İstanbul");
+sehirler2.Add("İstanbul");
 sehirler2.Add("İstanbul");
 Console.WriteLine(sehirler2.Count);
 
 class MyList<T>//GENERİC CLASS
 {
     T[] _array;
+    T[] _tempArray;
     public MyList()
     {
         _array = new T[0];
     }
     public void Add(T item)
     {
+        _tempArray = _array;
         _array=new T[_array.Length+1];
+        for (int i = 0; i < _tempArray.Length; i++)
+        {
+            _array[i] = _tempArray[i];
+        }
     }
 
 
